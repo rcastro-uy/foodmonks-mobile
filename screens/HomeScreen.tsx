5c8aaee2fcdaa83ae607bdc362f6cf7938ac1cc7
MauriIglesias <mauricio_iglesias@outlook.es>
2021-11-10 02:03:43 -0300

    Disenio del Home

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,38 +1,45 @@
 import React, { useContext, useEffect } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
-import { StatusBar } from "expo-status-bar";
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Keyboard } from "react-native";
 import { AuthContext } from "../context/AuthContext";
 import { listarRestauranteService } from "../services/listarRestaurantesService";
-import { Restaurante } from "../interfaces/AppInterfaces";
+import { categorias, Restaurante } from "../interfaces/AppInterfaces";
 import { RestauranteComponent } from "../components/Restaurante";
 import axios from "axios";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { API_URL } from "@env";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { Input, Switch } from "react-native-elements";
+import { Ionicons } from "@expo/vector-icons";
+import { fontPixel, pixelSizeHorizontal, pixelSizeVertical } from "../theme/Normalization";
+import { Picker } from "@react-native-picker/picker";
 
 export default function HomeScreen({navigation, route}:any) {
     const [restaurantes, setRestaurantes] = React.useState([]);
-    const [nombre, setNombre] = React.useState();
-    const [categoria, setCategoria] = React.useState();
-    const [orden, setOrden] = React.useState();
-    const { token, refreshToken } = useContext( AuthContext );
-    const data = {
-        nombre: nombre,
-        categoria: categoria,
-        orden: orden,
-    };
+    const [nombre, setNombre] = React.useState("");
+    const [categoria, setCategoria] = React.useState("");
+    const [orden, setOrden] = React.useState(false);
+    const { token, refreshToken, cerrarSesion } = useContext( AuthContext );
+
+    const onListarRestaurantes = () => {
+        Keyboard.dismiss();
+        console.log(`${nombre} + ${categoria} + ${orden}`)
+        listarRestauranteService(nombre, categoria, orden).then((data: any) => {
+            setRestaurantes(data);
+        });
+    }
+
+    const toggleSwitch = () => setOrden(previousState => !previousState);
+    
     useEffect(() => {
         console.log(`Bearer ${token}`);
         console.log(`Bearer ${refreshToken}`);
-        const requestListarRestaurantes = axios({
+        axios({
             method: "GET",
             url: `${API_URL}/v1/cliente/listarAbiertos?nombre=${nombre}&categoria=${categoria}&orden=${orden}`,
-            data: data,
             headers: {
                 Authorization: `Bearer ${token}`,
-                'RefreshAuthentication': `Bearer ${refreshToken}`,
+                RefreshAuthentication: `Bearer ${refreshToken}`,
             },
-        });
-        requestListarRestaurantes.then((res: any) => {
+        }).then((res: any) => {
             if (res.status === 200) {
                 console.log(res.data);
                 setRestaurantes(res.data);
@@ -40,29 +47,134 @@ export default function HomeScreen({navigation, route}:any) {
         }).catch((error) => {
             console.log(error);
         });
-        //restaurantesb = listarRestauranteService("","",true);
     }, [])
-    const { cerrarSesion } = useContext( AuthContext );
+    
     return (
-        <View style={styles.container}>
-            {restaurantes.map((item: Restaurante, index: number) => {
-                <RestauranteComponent nombre={item.nombre} descripcion={item.descripcion} imagen={item.imagen} calificacion={item.calificacion}/>
-            })}
-           {/*para probar el cierre de sesion */}
-            <Button 
-                title="cerrar sesion"
-                color="#5856D6"
-                onPress={ cerrarSesion }
-            />
+        <>
+        <View style={ styles.headerContainer } >
+        <Text style={ styles.title }>Restaurantes</Text>
+        {/* <Ionicons 
+        type='material-community'
+        name='arrow-back-circle'
+        size={35}
+        color={"#FD801E"}
+        onPress={ () => navigation.replace('Login') }
+        style={ styles.buttonReturn } /> */}
         </View>
+        <KeyboardAwareScrollView
+        contentContainerStyle={ styles.formContainer }
+        keyboardShouldPersistTaps='always'
+        >
+        <Input 
+            placeholder="Nombre del restaurante"
+            placeholderTextColor="rgba(255,80,40,0.3)"
+            inputContainerStyle={styles.inputField}
+            leftIcon={<Ionicons size={24} color={"#FD801E"} 
+            type={'font-awesome'} name="person"/>}
+            keyboardType="email-address"
+            selectionColor="black"
+            onChangeText = {setNombre}
+            value={nombre}
+            onSubmitEditing={ onListarRestaurantes }
+            autoCapitalize="none"
+            autoCorrect={ false }
+        />
+        <Picker
+        selectedValue={categoria}
+        onValueChange={(value, index) => setCategoria(value)}
+        mode="dropdown" // Android only
+        style={styles.picker}
+        >
+        <Picker.Item key={""} label={"(Cualquiera)"} value={""} />
+        {categorias.map((i, index) => 
+            <Picker.Item key={index} label={i.label} value={i.value} />
+        )}
+        </Picker>
+        <Text style={ {fontSize: fontPixel(15), color: 'black', marginLeft: pixelSizeHorizontal(340)} }>Orden</Text>
+        <Switch
+            trackColor={{ false: "#767577", true: "#767577" }}
+            thumbColor={orden ? "orange" : "#ffffff"}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleSwitch}
+            value={orden}
+        />
+        <TouchableOpacity
+            activeOpacity={ 0.8 }
+            style={ styles.button }
+            onPress={ onListarRestaurantes }
+        >
+            <Text style={ styles.buttonText } >Buscar</Text>
+        </TouchableOpacity>
+
+        {restaurantes.map((item: Restaurante, index: number) => {
+            <RestauranteComponent nombre={item.nombre} descripcion={item.descripcion} imagen={item.imagen} calificacion={item.calificacion}/>
+        })}
+
+        <RestauranteComponent nombre={"Mauricio"} descripcion={"el restaurante"} imagen={"img.com"} calificacion={4.5}/>
+        <RestauranteComponent nombre={"Mauricio"} descripcion={"el restaurante"} imagen={"img.com"} calificacion={3.2}/>
+        
+        {/* <Button 
+            title="cerrar sesion"
+            color="#5856D6"
+            onPress={ cerrarSesion }
+        /> */}
+        </KeyboardAwareScrollView>
+        </>
     )
 }
-
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    headerContainer: {
+        height: pixelSizeVertical(50),
+        marginTop: pixelSizeVertical(30),
+        borderBottomWidth:1
+    },
+    formContainer: {
+        top:-10,
+        marginTop:20,
+        paddingHorizontal: 11,
+        justifyContent:'center',
+    },
+    title: {
+        color: '#FD801E',
+        fontSize: fontPixel(25),
+        fontWeight: 'bold',
+        marginTop: 15,
+        textAlign: 'center'
+    },
+    button: {
+        borderWidth: 2,
+        borderColor: '#FD801E',
+        paddingHorizontal: 20,
+        paddingVertical: 5,
+        borderRadius: 100,
+        alignSelf:'center',
+        width: '45%',
+        marginBottom:20
+    },
+    buttonText: {
+        fontSize: 18,
+        color: '#FD801E',
+        alignSelf: 'center'
+    },
+    buttonReturn: {
+        position: 'absolute',
+        bottom: -3,
+        left: 5,
+    },
+    picker: {
+        marginVertical: 30,
+        width: 300,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: "#666",
+    },
+    inputField: {
+        borderBottomWidth: 1,
+        borderBottomColor:"black"
     },
 });
--- services/listarRestaurantesService.tsx
+++ services/listarRestaurantesService.tsx
@@ -1,33 +1,32 @@
 import { API_URL } from "@env";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
+import { useContext } from "react";
 import { Alert, Text, View } from "react-native";
+import { AuthContext } from "../context/AuthContext";
 import { RestauranteComp } from "../interfaces/AppInterfaces";
 
-export function listarRestauranteService(nombre: string, categoria: string, orden: boolean): any {
-    const token = AsyncStorage.getItem('token');
-    const refreshToken = AsyncStorage.getItem('refreshToken');
+export async function listarRestauranteService(nombre: string, categoria: string, orden: boolean) {
+    const { token, refreshToken } = useContext( AuthContext );
     console.log(token);
     console.log(`${API_URL}`)
-    const data = {
-        nombre: nombre,
-        categoria: categoria,
-        orden: orden,
-    };
-    const requestListarRestaurantes = axios({
+    // const data = {
+    //     nombre: nombre,
+    //     categoria: categoria,
+    //     orden: orden,
+    // };
+    await axios({
         method: "GET",
         url: `${API_URL}/v1/cliente/listarAbiertos?nombre=${nombre}&categoria=${categoria}&orden=${orden}`,
-        data: data,
+        //data: data,
         headers: {
             Authorization: `Bearer ${token}`,
             RefreshAuthentication: `Bearer ${refreshToken}`,
         },
-    });
-    requestListarRestaurantes.then((res) => {
+    }).then((res: any) => {
         if (res.status === 200) {
             return res.data;
         }
-    }).catch(err => {
+    }).catch((err: any) => {
         return (
             Alert.alert(
                 "Solicitud no realizada",
