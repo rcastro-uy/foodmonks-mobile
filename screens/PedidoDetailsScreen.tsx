608cd127219132d249d2b4086093a41fce6d3e4a
Antony <antonyh891@gmail.com>
2021-11-19 18:32:22 -0300

    Alta calificacion

--- screens/PedidoDetailsScreen.tsx
+++ screens/PedidoDetailsScreen.tsx
@@ -1,8 +1,11 @@
 import { StackScreenProps } from '@react-navigation/stack';
-import React, { useContext, useEffect } from 'react'
-import { Alert, Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
+import { Alert, Dimensions, LogBox, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import Toast from 'react-native-easy-toast';
 import { Button, Card, Icon, Image } from 'react-native-elements';
+import HighScore from '../components/HighScore';
 import { MenuCompraComp } from '../components/MenuCompraComp';
+import Modal from '../components/Modal';
 import { RootStackParams } from '../navigation/StackNavigator';
 import { pedidoDetailsStyles } from '../theme/PedidoDetailsTheme';
 
@@ -10,9 +13,21 @@ interface Props extends StackScreenProps<RootStackParams, 'PedidoDetailsScreen'>
 const {width : width} = Dimensions.get('screen');
 export default function PedidoDetailsScreen({navigation, route}:Props) {
 
+    const idPedido = route.params.idPedido;
+    const estadoPedido = route.params.estadoPedido;
+    const calificacionRestaurante = route.params.calificacionRestaurante; //Es true si el restaurante ya tiene calificacion
+    const menus = route.params.menus;
+
     const [cantidad, setCantidad] = React.useState<number>(0);
+    const toastRef = React.useRef<any>()
+    const [ refrescar, setRefrescar ] = useState( false );
+    const [ existeCalificacion, setExisteCalificacion ] = useState( calificacionRestaurante );
+    const [mostrarModal, setMostrarModal] = useState(false)
+    const [renderComponent, setRenderComponent] = useState(<HighScore />)
+
     
     useEffect(() => {
+        LogBox.ignoreLogs(['VirtualizedLists should never be nested'])
         navigation.setOptions({
             title:'Pedido',
             headerTitleAlign:'center',
@@ -21,37 +36,52 @@ export default function PedidoDetailsScreen({navigation, route}:Props) {
             headerBackTitleStyle:{color:'black'},
             headerTintColor: 'black',
         })
-        //setCantidad(0)
-    }, [])
+        setRefrescar(false)
+
+    }, [refrescar])
 
     //const { agregarProducto} = useContext( CarritoContext );
 
-    const idPedido = route.params.idPedido;
-    const estadoPedido = route.params.estadoPedido;
-    const calificacionRestaurante = route.params.calificacionRestaurante; //Es true si el restaurante ya tiene calificacion
-    const menus = route.params.menus;
+    const altaBajaModificarPuntuacion =() => {
+         
+        if (!existeCalificacion) {
+            setRenderComponent(
+                <HighScore
+                toastRef={toastRef} idPedido={idPedido} setMostrarModal={setMostrarModal} setExisteCalificacion={setExisteCalificacion} setRefrescar ={setRefrescar}
+                />
+            )
+            setMostrarModal(true)
+        }
+
+    }
 
     return (
-      <>             
-        <View style={{flex:0.13, top:50}}>
-        <Button
-            // icon={
-            //     <Icon
-            //         name="plus"
-            //         type="material-community"
-            //         size={25}
-            //         color="#FD801E"
-            //         style={{alignItems:'center'}}
-            //     />
-            // }
-            type="outline"
-            buttonStyle = {pedidoDetailsStyles.button}
-            activeOpacity={ 0.8 }
-            style={ pedidoDetailsStyles.button }
-            //onPress={() => agregarAlCarrito()}
-            title = 'Calificar'
-            titleStyle= {pedidoDetailsStyles.title}
-        />
+      <>       
+            
+        <View style={{flex:0.2, justifyContent:'space-evenly'}}>
+        {
+            (estadoPedido == 'Rechazado' || estadoPedido == 'Finalizado' || estadoPedido == 'Devuelto') ?
+            (
+
+                <Button
+                    // icon={
+                    //     <Icon
+                    //         name="plus"
+                    //         type="material-community"
+                    //         size={25}
+                    //         color="#FD801E"
+                    //         style={{alignItems:'center'}}
+                    //     />
+                    // }
+                    type="outline"
+                    buttonStyle = {pedidoDetailsStyles.button}
+                    activeOpacity={ 0.8 }
+                    style={ pedidoDetailsStyles.button }
+                    onPress={() => altaBajaModificarPuntuacion()}
+                    title = {(existeCalificacion== false)? ('Calificar'):('Modificar/Eliminar Calificacion')}
+                    titleStyle= {pedidoDetailsStyles.title}
+                />
+            ):(null )}
             <Button
             // icon={
             //     <Icon
@@ -70,13 +100,20 @@ export default function PedidoDetailsScreen({navigation, route}:Props) {
             title = 'Reclamar'
             titleStyle= {pedidoDetailsStyles.title}
         />
+           
         </View>
-        <View style={{ flex: 0.7,backgroundColor:"#f2f2f2", marginTop: 30 }}>           
+        <View style={{ flex: 0.7,backgroundColor:"#f2f2f2"}}>           
             <View style={{width: width, padding:15, alignItems:'center' }}>
               <Text style={{fontWeight: 'bold', fontSize:20,paddingStart:5}}> Menus</Text>
               <MenuCompraComp vertical={true} productos={menus} width={150} height={250} />         
             </View>
-          </View>
+            <Modal visible={mostrarModal} setVisible={setMostrarModal}>
+                {
+                    renderComponent
+                }
+            </Modal>
+        </View>
+        <Toast ref={toastRef} position="bottom" opacity={0.9}/>    
     </>
     )
 }
